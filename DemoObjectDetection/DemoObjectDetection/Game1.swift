86c79066e65d417a7fbb371de94cdc086162db86
hoaithi <mailthi216@gmail.com>
2020-05-07 13:59:59 +0700

    first push

---
+++ DemoObjectDetection/DemoObjectDetection/Game1.swift
@@ -0,0 +1,33 @@
+//
+//  Game1.swift
+//  DemoObjectDetection
+//
+//  Created by vincent on 5/7/20.
+//  Copyright © 2020 Vincent_ Nguyen Hoai Thi. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class Game1: UIViewController {
+    private var _sceneView: SCNView!
+    private var _gameScene: SCNScene!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
