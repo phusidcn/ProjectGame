ae8514307f28a5132d338e5adaacbe8729f80c52
Thi <hoaithipm2@gmail.com>
2020-07-22 14:23:09 +0700

    [ app ] newdiection

--- DemoObjectDetection/DemoObjectDetection/GameController/HandlerBlock.swift
+++ DemoObjectDetection/DemoObjectDetection/GameController/HandlerBlock.swift
@@ -29,38 +29,38 @@ extension GameController: ObjectsRecognitionDelegate {
         }
     }
     
-    func actionSequenceDidChange(actions: [UserStep]) {
-        let needToExecute: Bool = actions.contains(where: {userstep in
-            return userstep.action == .Pressed
-        })
-        if needToExecute {
-            actions.forEach({userStep in
-                let repeatTime = repeatNumberOf(action: userStep)
-                print("\(userStep.action) \(userStep.number)")
-                for _ in 0 ..< repeatTime {
-                    switch userStep.action {
-                    case .Walk_Up:
-                        character?.moveByPosition(direction: .forward)
-                    case .Walk_Down:
-                        character?.moveByPosition(direction: .backward)
-                    case .Walk_Left:
-                        character?.moveByPosition(direction: .left)
-                    case .Walk_Right:
-                        character?.moveByPosition(direction: .right)
-                    case .Jump_Up:
-                        character?.jumpByPosition(direction: .forward)
-                    case .Jump_Down:
-                        character?.jumpByPosition(direction: .backward)
-                    case .Jump_Left:
-                        character?.jumpByPosition(direction: .left)
-                    case .Jump_Right:
-                        character?.jumpByPosition(direction: .right)
-                    default:
-                        break
-                    }
-                }
-            })
-        }
-        print("==================================================")
-    }
+//    func actionSequenceDidChange(actions: [UserStep]) {
+//        let needToExecute: Bool = actions.contains(where: {userstep in
+//            return userstep.action == .Pressed
+//        })
+//        if needToExecute {
+//            actions.forEach({userStep in
+//                let repeatTime = repeatNumberOf(action: userStep)
+//                print("\(userStep.action) \(userStep.number)")
+//                for _ in 0 ..< repeatTime {
+//                    switch userStep.action {
+//                    case .Walk_Up:
+//                        character?.moveByPosition(direction: .forward)
+//                    case .Walk_Down:
+//                        character?.moveByPosition(direction: .backward)
+//                    case .Walk_Left:
+//                        character?.moveByPosition(direction: .left)
+//                    case .Walk_Right:
+//                        character?.moveByPosition(direction: .right)
+//                    case .Jump_Up:
+//                        character?.jumpByPosition(direction: .forward)
+//                    case .Jump_Down:
+//                        character?.jumpByPosition(direction: .backward)
+//                    case .Jump_Left:
+//                        character?.jumpByPosition(direction: .left)
+//                    case .Jump_Right:
+//                        character?.jumpByPosition(direction: .right)
+//                    default:
+//                        break
+//                    }
+//                }
+//            })
+//        }
+//        print("==================================================")
+//    }
 }
