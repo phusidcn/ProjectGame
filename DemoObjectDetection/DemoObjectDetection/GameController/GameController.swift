a9ca9dc26584ac5a246007ea565ae6194721ce7e
Thi <hoaithipm2@gmail.com>
2020-07-28 20:08:56 +0700

    [ game ] level3
    - description (if have)

--- DemoObjectDetection/DemoObjectDetection/GameController/GameController.swift
+++ DemoObjectDetection/DemoObjectDetection/GameController/GameController.swift
@@ -392,33 +392,45 @@
             scene!.rootNode.addAudioPlayer(SCNAudioPlayer(source: audioSources[audioName.rawValue]))
         }
         
-        func setupAudio() {
-            // Get an arbitrary node to attach the sounds to.
-            let node = scene!.rootNode
-            
-            // ambience
-            if let audioSource = SCNAudioSource(named: "audio/ambience.mp3") {
-                audioSource.loops = true
-                audioSource.volume = 0.8
-                audioSource.isPositional = false
-                audioSource.shouldStream = true
-                node.addAudioPlayer(SCNAudioPlayer(source: audioSource))
-            }
-            // volcano
-            
-            
-            // other sounds
-            audioSources[AudioSourceKind.collect.rawValue] = SCNAudioSource(named: "audio/collect.mp3")!
-            audioSources[AudioSourceKind.collectBig.rawValue] = SCNAudioSource(named: "audio/collectBig.mp3")!
-            
-            
-            // adjust volumes
-            audioSources[AudioSourceKind.collect.rawValue].isPositional = false
-            audioSources[AudioSourceKind.collectBig.rawValue].isPositional = false
-            
-            audioSources[AudioSourceKind.collect.rawValue].volume = 4.0
-            audioSources[AudioSourceKind.collectBig.rawValue].volume = 4.0
-        }
+        
+             func setupAudio() {
+                  // Get an arbitrary node to attach the sounds to.
+                  let node = scene!.rootNode
+
+                  // ambience
+                  if let audioSource = SCNAudioSource(named: "audio/ambience.mp3") {
+                      audioSource.loops = true
+                      audioSource.volume = 0.8
+                      audioSource.isPositional = false
+                      audioSource.shouldStream = true
+                      node.addAudioPlayer(SCNAudioPlayer(source: audioSource))
+                  }
+                  // volcano
+                  if let volcanoNode = scene!.rootNode.childNode(withName: "particles_volcanoSmoke_v2", recursively: true) {
+                      if let audioSource = SCNAudioSource(named: "audio/volcano.mp3") {
+                          audioSource.loops = true
+                          audioSource.volume = 5.0
+                          volcanoNode.addAudioPlayer(SCNAudioPlayer(source: audioSource))
+                      }
+                  }
+
+                  // other sounds
+                  audioSources[AudioSourceKind.collect.rawValue] = SCNAudioSource(named: "audio/collect.mp3")!
+                  audioSources[AudioSourceKind.collectBig.rawValue] = SCNAudioSource(named: "audio/collectBig.mp3")!
+//                  audioSources[AudioSourceKind.unlockDoor.rawValue] = SCNAudioSource(named: "audio/unlockTheDoor.m4a")!
+//                  audioSources[AudioSourceKind.hitEnemy.rawValue] = SCNAudioSource(named: "audio/hitEnemy.wav")!
+
+                  // adjust volumes
+//                  audioSources[AudioSourceKind.unlockDoor.rawValue].isPositional = false
+                  audioSources[AudioSourceKind.collect.rawValue].isPositional = false
+                  audioSources[AudioSourceKind.collectBig.rawValue].isPositional = false
+//                  audioSources[AudioSourceKind.hitEnemy.rawValue].isPositional = false
+
+//                  audioSources[AudioSourceKind.unlockDoor.rawValue].volume = 0.5
+                  audioSources[AudioSourceKind.collect.rawValue].volume = 4.0
+                  audioSources[AudioSourceKind.collectBig.rawValue].volume = 4.0
+              }
+        
         
         // MARK: - Init
         
@@ -543,12 +555,14 @@
                     
                     // update the overlay
                     self.overlay?.collectedGemsCount = self.collectedGems
-                    startCinematic()
-                    if let scenView = self.sceneRenderer as? SCNView {
-                        scenView.allowsCameraControl = false
-                    }
-                    if collectedGems == 1 {
+                    
+               
+                    if collectedGems == 4 {
                         //TODO : handle win game
+                        startCinematic()
+                        if let scenView = self.sceneRenderer as? SCNView {
+                                           scenView.allowsCameraControl = false
+                                       }
                         GameStorage.currentLevel = self.currentLevel
                         GameStorage.storePoint(point: self.point)
                         GameStorage.saveGame()
